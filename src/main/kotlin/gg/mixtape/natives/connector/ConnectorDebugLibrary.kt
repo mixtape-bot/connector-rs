0eacd67680d5a76f606d0b95e5002106f25d7b32
melike2d <gino@femboy.email>
2022-03-10 00:35:17 -0800

    refactor: remove debug code

--- src/main/kotlin/gg/mixtape/natives/connector/ConnectorDebugLibrary.kt
+++
@@ -1,5 +0,0 @@
-package gg.mixtape.natives.connector
-
-object ConnectorDebugLibrary {
-    external fun configureLogging()
-}
--- src/main/rust/connector/debug.rs
+++
@@ -1,7 +0,0 @@
-use jni::JNIEnv;
-use jni::objects::JClass;
-
-#[no_mangle]
-pub extern "system" fn Java_gg_mixtape_natives_connector_ConnectorDebugLibrary_configureLogging(_: JNIEnv, _: JClass) {
-    env_logger::init();
-}
